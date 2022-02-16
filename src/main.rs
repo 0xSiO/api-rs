77604c3840039fc409ce37de76ed4c5a60c799f3
Luc Street <6709262+0xSiO@users.noreply.github.com>
2022-02-15 22:00:05 -0800

    Separate routes into new module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use axum::{routing::get, Router};
 use axum_extra::middleware::from_fn;
 use tower::ServiceBuilder;
 use tracing::*;
 use tracing_subscriber::EnvFilter;
 
 pub mod middleware;
+pub mod routing;
 
 #[tokio::main]
 async fn main() {
@@ -14,13 +14,11 @@ async fn main() {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .layer(
-            ServiceBuilder::new()
-                .layer(from_fn(middleware::request_id::middleware))
-                .layer(middleware::tracing::middleware()),
-        );
+    let app = routing::router().layer(
+        ServiceBuilder::new()
+            .layer(from_fn(middleware::request_id::middleware))
+            .layer(middleware::tracing::middleware()),
+    );
 
     info!("starting server");
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
