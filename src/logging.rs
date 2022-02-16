bae8d169844cab05d4c787f758912fe23dad75bc
Luc Street <6709262+0xSiO@users.noreply.github.com>
2022-02-15 20:11:04 -0800

    Customize tracing middleware span

--- src/logging.rs
+++ src/logging.rs
@@ -1,9 +1,26 @@
+use axum::http::Request;
 use tower_http::{
     classify::{ServerErrorsAsFailures, SharedClassifier},
-    trace::{DefaultMakeSpan, DefaultOnFailure, DefaultOnRequest, DefaultOnResponse, TraceLayer},
+    trace::{DefaultOnFailure, DefaultOnRequest, DefaultOnResponse, MakeSpan, TraceLayer},
 };
-use tracing::Level;
+use tracing::{Level, Span};
 use tracing_subscriber::EnvFilter;
+
+#[derive(Clone, Copy)]
+pub struct CustomMakeSpan;
+
+impl<B> MakeSpan<B> for CustomMakeSpan {
+    fn make_span(&mut self, request: &Request<B>) -> Span {
+        tracing::info_span!(
+            "request",
+            method = %request.method(),
+            uri = %request.uri(),
+            http_version = ?request.version(),
+            headers = ?request.headers(),
+            api_version = env!("CARGO_PKG_VERSION"),
+        )
+    }
+}
 
 pub fn init() {
     tracing_subscriber::fmt()
@@ -12,13 +29,9 @@ pub fn init() {
         .init();
 }
 
-pub fn middleware() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>> {
+pub fn middleware() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>, CustomMakeSpan> {
     TraceLayer::new_for_http()
-        .make_span_with(
-            DefaultMakeSpan::new()
-                .level(Level::INFO)
-                .include_headers(true),
-        )
+        .make_span_with(CustomMakeSpan)
         .on_request(DefaultOnRequest::new().level(Level::INFO))
         .on_response(
             DefaultOnResponse::new()
