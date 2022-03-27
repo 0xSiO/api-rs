b5bfb90a691f66f81f8d9fd15b6624c3ea36448e
Luc Street <6709262+0xSiO@users.noreply.github.com>
2022-03-26 23:40:04 -0700

    Add shared state extension layer with a PgPool

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use axum::middleware::from_fn;
+use axum::{extract::Extension, middleware::from_fn};
+use sqlx::PgPool;
 use tower::ServiceBuilder;
 use tracing_subscriber::EnvFilter;
 
 pub mod error;
 pub mod middleware;
 pub mod route;
+
+#[derive(Debug, Clone)]
+struct State {
+    db: PgPool,
+}
 
 #[tokio::main]
 async fn main() {
@@ -14,10 +20,17 @@ async fn main() {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
+    let db = PgPool::connect(&dotenv::var("DATABASE_URL").unwrap())
+        .await
+        .unwrap();
+
+    let state = State { db };
+
     let app = route::router().layer(
         ServiceBuilder::new()
             .layer(from_fn(middleware::request_id))
-            .layer(from_fn(middleware::trace)),
+            .layer(from_fn(middleware::trace))
+            .layer(Extension(state)),
     );
 
     let server =
